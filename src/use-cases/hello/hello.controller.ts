be7c3ef280902ca2d9d0098c9d513a6c324e8d46
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2022-04-07 18:07:12 -0300

    chore: setup exampple route

---
+++ src/use-cases/hello/hello.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@Controller('hello')
+export class HelloController {
+  @Get('/')
+  @ApiTags('Hello')
+  @ApiOperation({
+    summary: 'Hello',
+    description: 'Hello',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: {
+      type: 'Object',
+      example: {
+        statusCode: 200,
+        message: 'Hello',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error',
+    schema: {
+      type: 'Object',
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  })
+  hello() {
+    return {
+      statusCode: 200,
+      message: 'Hello',
+    };
+  }
+}
